aa991e629df1bc2a771aa2c5e70e595cf7ca743b
David Andersson <david.andersson@zocom.se>
2021-09-07 14:21:37 +0200

    Adding sessions

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,30 @@
+// importera Express + middleware
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const passport = require('passport')
+
+// Konfigurera servern
+const app = express()
+const PORT: number = 1337
+let requestCount: number = 0
+
+app.use( express.urlencoded() )
+app.use( express.json() )
+app.use( (req: Request, res: Response, next: any) => {
+	requestCount++
+	console.log(`${requestCount}  ${req.method}  ${req.url} `, req.body)
+	next()
+} )
+
+app.use(cookieParser())
+app.use(expressSession())
+// app.use( passport ? )
+
+// Endpoints
+
+
+// Starta servern
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}.`)
+})
